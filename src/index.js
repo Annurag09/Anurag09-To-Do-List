7c433a6ac7d583a13faece6d5ab8e0bb5394e62c
Annurag09 <46589691+Annurag09@users.noreply.github.com>
2022-01-10 00:17:15 +0530

    Added global data & storage

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./devGlobal";
+import "./storage";
+
+ReactDOM.render(<App />, document.getElementById("root"));
